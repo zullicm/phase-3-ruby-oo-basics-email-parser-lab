f885fb00182d1adb1044fa31687c76eef6b5ca87
ihollander <ianholla@gmail.com>
2021-07-11 08:24:51 -0400

    Added description of regular expression

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,6 +1,7 @@
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
+# or whitespace (' ').
 
 class EmailAddressParser
   attr_reader :emails
@@ -10,6 +11,11 @@ class EmailAddressParser
   end
 
   def parse
+    # the "/, | /" code below is a regular expression
+    # the beginning and end of a regular expression is defined by //
+    # within the regular expression, we are identifying two possible strings to split on:
+    # ", " or " "
+    # the pipe | represents OR in a regular expression 
     emails.split(/, | /).uniq
   end
 end
